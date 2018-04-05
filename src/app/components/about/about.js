5152a0a8281d95dfdadd6bbe9e3bf13d6165c251
fomicha <fomich.aleksandr@gmail.com>
2018-04-05 18:29:30 +0300

    about start

---
+++ src/app/components/about/about.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './about.scss';
+import '../common/commonStyles.scss';
+
+class About extends Component {
+    render() {
+        return (
+            <div className="wrapper">
+                <div className="hz">
+                    <h1>hsdsf</h1>
+                    <p>hjggj</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default About;
