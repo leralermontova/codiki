f3f400232e38a354ad5e4b26a70d6027e42a33f7
fomicha <fomich.aleksandr@gmail.com>
2018-04-05 18:18:57 +0300

    about start

--- src/app/components/cookies/Cookies.js
+++ src/app/components/cookies/Cookies.js
@@ -13,12 +13,12 @@ class Cookies extends Component {
               <nav className="header-nav">
                 <ul className="nav-list">
                   <li className="nav-list__item"><a className="active nav-list__link" href="#"> <span className="none-after">O nas</span></a></li>
-                  <li className="nav-list__item"><a className="nav-list__link" href="#"> <span>Opisanie Oferty</span></a></li>
-                  <li className="nav-list__item"><a className="nav-list__link" href="#"> <span>Finansowanie</span></a></li>
-                  <li className="nav-list__item"><a className="nav-list__link" href="#"> <img src={logo} alt="dom" /></a></li>
-                  <li className="nav-list__item"><a className="nav-list__link" href="#"> <span>Localizacja</span></a></li>
-                  <li className="nav-list__item"><a className="nav-list__link" href="#"> <span>Oferta</span></a></li>
-                  <li className="nav-list__item"><a className="nav-list__link" href="#"> <span>KontaKt</span></a></li>
+                  <li className="nav-list__item"><a className="nav-list__link" href=""> <span>Opisanie Oferty</span></a></li>
+                  <li className="nav-list__item"><a className="nav-list__link" href=""> <span>Finansowanie</span></a></li>
+                  <li className="nav-list__item"><a className="nav-list__link" href=""> <img src={logo} alt="dom" /></a></li>
+                  <li className="nav-list__item"><a className="nav-list__link" href=""> <span>Localizacja</span></a></li>
+                  <li className="nav-list__item"><a className="nav-list__link" href=""> <span>Oferta</span></a></li>
+                  <li className="nav-list__item"><a className="nav-list__link" href=""> <span>KontaKt</span></a></li>
                 </ul>
               </nav>
               <div className="menu-icon"><span></span></div>
--- src/app/routes.js
+++ src/app/routes.js
@@ -1,18 +1,19 @@
-// import Home from "../app/components/home/Home";
-import News from "../app/components/news/News";
 import Cookies from "../app/components/cookies/Cookies";
-
+import About from "../app/components/about/about";
 const routes = [
 
-  {
-    path: "/news",
-    component: News
-  },
   {
     path: "/",
     exact: true,
     component: Cookies
-  }
+  },
+
+    {
+        path: "/",
+        exact: true,
+        component: About
+    }
+
 ];
 
 export default routes;
