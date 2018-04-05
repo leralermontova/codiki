6f5333cce127d4a5be533003fd56a92698208f63
Валерия Цыбулько <v.tsybulko@betinvest.com>
2018-04-05 19:08:32 +0300

    carousel

--- src/app/components/carousel/Carousel.js
+++ src/app/components/carousel/Carousel.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component} from "react";
 import Slider from "react-slick";
 import './Carousel.scss';
 import img1 from './images/1.png';
@@ -6,32 +6,44 @@ import img2 from './images/2.png';
 import img3 from './images/3.png';
 import img4 from './images/4.png';
 
-class Carousel extends React.Component {
+export default class Carousel extends Component {
+
+  constructor(props) {
+    super(props);
+    this.play = this.play.bind(this);
+    this.pause = this.pause.bind(this);
+  }
+  play() {
+    this.slider.slickPlay();
+  }
+  pause() {
+    this.slider.slickPause();
+  }
   render() {
     var settings = {
       dots: true,
       infinite: true,
       speed: 500,
       slidesToShow: 1,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
     };
     return (
-      <Slider {...settings}>
-        <div style={{width: 100, height: 100}}>
-          jkjkjk
+      <Slider ref={slider => (this.slider = slider)} {...settings} className="slick">
+        <div>
+          <img src={img1} alt="" />
         </div>
         <div>
           <img src={img2} alt="" />
         </div>
         <div>
-          <img src={img4} alt="" />
+          <img src={img3} alt="" />
         </div>
         <div>
-          <img src={img3} alt="" />
+          <img src={img4} alt="" />
         </div>
       </Slider>
     );
   }
 }
-
-export default Carousel;
