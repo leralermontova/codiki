370b5e6a402cc3dc835d8b07bd96d055b0e9e68b
fomicha <fomich.aleksandr@gmail.com>
2018-04-07 02:30:15 +0300

    about 1920 almost done

--- src/app/components/about/about.js
+++ src/app/components/about/about.js
@@ -2,14 +2,62 @@ import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
 import './about.scss';
 import '../common/commonStyles.scss';
+import img1 from './img/photo1.png'
+import img2 from './img/photo2.png'
+import icon1 from './img/1.svg'
+import icon2 from './img/2.svg'
+import icon3 from './img/3.svg'
+
 
 class About extends Component {
     render() {
         return (
-            <div className="wrapper">
-                <div className="hz">
-                    <h1>hsdsf</h1>
-                    <p>hjggj</p>
+            <div className="about">
+                <div className="wrapper">
+                    <div className="about-inner">
+                        <div className="about-inner__header">
+                            <h1>O nas</h1>
+                        </div>
+                        <div className="about-inner__item-wrap">
+                            <div className="text-container">
+                                <h2><strong>Lake</strong> village</h2>
+                                <p>To nowoczesne osiedle o zabudowie
+                                    willowej w podwyższonym standardzie
+                                    położone w cichej i bezpiecznej
+                                    okolicy pod Warszawą, w Nowej Woli
+                                </p>
+                            </div>
+                            <div className="img-right">
+                                <img src={img1} alt=""/>
+                            </div>
+                            <div className="img-left">
+                                <img src={img2} alt=""/>
+                            </div>
+                            <div className="text-container second">
+                                <h2><strong>Lake</strong> village</h2>
+                                <p>To nowoczesne osiedle o zabudowie
+                                    willowej w podwyższonym standardzie
+                                    położone w cichej i bezpiecznej
+                                    okolicy pod Warszawą, w Nowej Woli
+                                </p>
+                            </div>
+                            <div className="square">
+                                <div className="square__icon">
+                                    <img src={icon1} alt=""/>
+                                </div>
+                                <div className="square__icon">
+                                    <img src={icon2} alt=""/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="footer-wrap">
+                    <div className="footer-wrap__img">
+                        <img src={icon3} alt=""/>
+                    </div>
+                    <div className="footer-wrap__line">
+                    </div>
                 </div>
             </div>
         );
