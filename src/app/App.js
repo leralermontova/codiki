641fe5527d4aaad69e65e0909ee43204c13c461d
Валерия Цыбулько <v.tsybulko@betinvest.com>
2018-04-09 16:11:41 +0300

    fixes

--- src/app/App.js
+++ src/app/App.js
@@ -2,17 +2,16 @@ import React from 'react';
 import Route from 'react-router-dom/Route';
 import Switch from 'react-router-dom/Switch';
 import Header from "../app/components/header/Header";
-import Cookies from "../app/components/cookies/Cookies";
+// import Cookies from "../app/components/cookies/Cookies";
 import routes from './routes';
 import './App.scss';
 
 const App = () => (
-  <div>
+  <div className="app">
     <Switch>
       {routes.map((route, i) => <Route key={i} {...route} />)}
     </Switch>
     <Header />
-    <Cookies />
   </div>
 );
 
--- src/app/components/header/Header.js
+++ src/app/components/header/Header.js
@@ -1,30 +1,64 @@
 import React, { Component } from 'react';
+import MediaQuery from 'react-responsive';
 import { Link } from 'react-router-dom';
 import './Header.scss';
 import logo from './images/Logoheader.svg';
 
 class Cookies extends Component {
+
+    state = {
+    isOpen: false,
+  };
+
+  toggleMenu = () => this.setState({ isOpen: !this.state.isOpen });
+
+
   render() {
     return (
       <div className="header">
-        <div className="header-wraper">
-          <div className="header-inner">
-            <div className="header-nav-wrap">
-              <nav className="header-nav">
-                <ul className="nav-list">
-                  <li className="nav-list__item"><a className="active nav-list__link" href="#"> <span className="none-after">O nas</span></a></li>
-                  <li className="nav-list__item"><a className="nav-list__link" href="#"> <span>Opisanie Oferty</span></a></li>
-                  <li className="nav-list__item"><a className="nav-list__link" href="#"> <span>Finansowanie</span></a></li>
-                  <li className="nav-list__item"><a className="nav-list__link" href="#"> <img src={logo} alt="dom" /></a></li>
-                  <li className="nav-list__item"><a className="nav-list__link" href="#"> <span>Localizacja</span></a></li>
-                  <li className="nav-list__item"><a className="nav-list__link" href="#"> <span>Oferta</span></a></li>
-                  <li className="nav-list__item"><a className="nav-list__link" href="#"> <span>KontaKt</span></a></li>
-                </ul>
-              </nav>
-              <div className="menu-icon"><span></span></div>
+        <MediaQuery minDeviceWidth={1024}>
+          <div className="header-wraper">
+            <div className="header-inner">
+              <div className="header-nav-wrap">
+                <nav className="header-nav">
+                  <ul className="nav-list">
+                    <li className="nav-list__item"><a className="active nav-list__link"> <span className="none-after">O nas</span></a></li>
+                    <li className="nav-list__item"><a className="nav-list__link"> <span>Opisanie Oferty</span></a></li>
+                    <li className="nav-list__item"><a className="nav-list__link"> <span>Finansowanie</span></a></li>
+                    <li className="nav-list__item"><a className="nav-list__link"> <img src={logo} alt="dom" /></a></li>
+                    <li className="nav-list__item"><a className="nav-list__link"> <span>Localizacja</span></a></li>
+                    <li className="nav-list__item"><a className="nav-list__link"> <span>Oferta</span></a></li>
+                    <li className="nav-list__item"><a className="nav-list__link"> <span>KontaKt</span></a></li>
+                  </ul>
+                </nav>
+              </div>
             </div>
           </div>
-        </div>
+        </MediaQuery>
+        <MediaQuery maxDeviceWidth={1023}>
+          <div className={"header-wraper " +(this.state.isOpen ? ' open' : 'close')} onClick={this.toggleMenu}>
+            <div className="header-inner">
+              <div className="header-nav-wrap">
+                <nav className="header-nav">
+                  <ul className="nav-list">
+                    <li className="nav-list__item"><a className="active nav-list__link"> <span className="none-after">O nas</span></a></li>
+                    <li className="nav-list__item"><a className="nav-list__link"> <span>Opisanie Oferty</span></a></li>
+                    <li className="nav-list__item"><a className="nav-list__link"> <span>Finansowanie</span></a></li>
+                    <li className="nav-list__item"><a className="nav-list__link"> <img src={logo} alt="dom" /></a></li>
+                    <li className="nav-list__item"><a className="nav-list__link"> <span>Localizacja</span></a></li>
+                    <li className="nav-list__item"><a className="nav-list__link"> <span>Oferta</span></a></li>
+                    <li className="nav-list__item"><a className="nav-list__link"> <span>KontaKt</span></a></li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          </div>
+          <div
+            className = {"menu-icon " +(this.state.isOpen ? ' open' : 'close')}
+            onClick={this.toggleMenu}>
+              <span></span>
+          </div>
+        </MediaQuery>
       </div>
     );
   }
