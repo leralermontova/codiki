f45ca759c43a0f0468056c174a37b87f935e6b83
Валерия Цыбулько <v.tsybulko@betinvest.com>
2018-04-11 16:35:33 +0300

    ololo

--- src/app/components/podklucz/PodKlucz.js
+++ src/app/components/podklucz/PodKlucz.js
@@ -4,6 +4,7 @@ import './PodKlucz.scss';
 import img1 from './images/klucz1.jpg';
 import img2 from './images/klucz2.jpg';
 import img3 from './images/klucz3.jpg';
+import radio from './images/radio.png';
 
 export default class PodKlucz extends Component {
 
@@ -25,18 +26,43 @@ export default class PodKlucz extends Component {
       slidesToShow: 1,
       slidesToScroll: 1,
       autoplay: true,
-      autoplaySpeed: 2000
+      autoplaySpeed: 2000,
+      dots: true,
+      appendDots: dots => (
+        <div
+          style={{
+            bottom: "20px"
+          }}
+        >
+          <ul style={{ margin: "0px" }}>
+            {dots}
+           </ul>
+        </div>
+      ),
+      customPaging: i => (
+        <div
+          style={{
+            width: "50px"
+          }}
+        >
+          <img src={radio} alt="" className="radio" />
+        </div>
+      )
     };
     return (
-      <div>
+      <div className="podKlucz">
         <div className="titleBlock">
-          <div className="mainText">Pod klucz</div>
-          <div className="secondaryText">Pod klucz <br/>Pod klucz <br/>Pod klucz </div>
+          <div className="mainText">POD KLUCZ</div>
+          <div className="secondaryText">
+            Oferujemy możliwość wykończenia<br/>
+            zakupionych przez Państwa <br/>
+            lokali pod klucz.
+          </div>
         </div>
         <div className="horizontal">
           <div className="mainContainer">
-            <div className="title"></div>
-            <div className="subtitle"> <br/></div>
+            <div className="title">SPRAWDZENI FACHOWCY</div>
+            <div className="subtitle">GWARANTUJĄ SOLIDNIE <br/>TERMINDWO WYKONANE PRACE</div>
             <div className="infoText">
               <div className="secondaryText">
                 <span>Wiemy, że bywają sytuacje kiedy mają</span>
@@ -45,9 +71,9 @@ export default class PodKlucz extends Component {
                 <span>nie stanowi dla nas problemu.</span>
                 <span>Decydując się na wykończenie</span>
                 <span>z nami wnętrza:</span>
-                <span>@ oszczędzają Państwo czas</span>
-                <span>@ współpracują Państwo ze specjalistami</span>
-                <span>@ mogą Państwo liczyć na porady</span>
+                <span>• oszczędzają Państwo czas</span>
+                <span>• współpracują Państwo ze specjalistami</span>
+                <span>• mogą Państwo liczyć na porady</span>
               </div>
             </div>
           </div>
