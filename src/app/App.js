4be640b8bf65321bc0faddd5c2653130106dda90
Валерия Цыбулько <v.tsybulko@betinvest.com>
2018-04-12 12:14:00 +0300

    ---> in App

--- src/app/App.js
+++ src/app/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import Route from 'react-router-dom/Route';
 import Switch from 'react-router-dom/Switch';
 import Header from "../app/components/header/Header";
+import Carousel from "../app/components/carousel/Carousel";
+import About from "../app/components/about/About";
 import PodKlucz from "../app/components/podklucz/PodKlucz";
-// import Cookies from "../app/components/cookies/Cookies";
+import Cookies from "../app/components/cookies/Cookies";
 import routes from './routes';
 import './App.scss';
 
@@ -13,7 +15,10 @@ const App = () => (
       {routes.map((route, i) => <Route key={i} {...route} />)}
     </Switch>
     <Header />
+    <Carousel />
+    <About />
     <PodKlucz />
+    <Cookies />
   </div>
 );
 
