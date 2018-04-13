8a3b5eb858cc68ba2615d511a1a493168bffaf6e
Валерия Цыбулько <v.tsybulko@betinvest.com>
2018-04-13 09:49:56 +0300

    oferta

---
+++ src/app/components/oferta/Oferta.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './oferta.scss';
+
+class Oferta extends Component {
+  render() {
+    return (
+      <div className="local">
+        <div className="titleBlock">
+          <div className="mainText">Localizacja</div>
+          <div className="secondaryText">
+            Cicha i bezpieczna okolica sprawa, ze mne mneszqma oraz domy przeznaczone na sprzedaz<br/>
+            na Mazowszu nie sa w stanie konkurować z tą localizacja. <br/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Oferta;
