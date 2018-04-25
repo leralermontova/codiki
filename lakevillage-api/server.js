319f880262d264871ffafe6267473c63b7f12369
Ivan <iqsk81ad@gmail.com>
2018-04-25 15:48:39 +0300

    #presave

--- lakevillage-api/server.js
+++ lakevillage-api/server.js
@@ -1,18 +1,22 @@
 var express = require('express'),
   app = express(),
-  port =  3001,
+  port =  4000,
   mongoose = require('mongoose'),
   House = require('./api/models/lakevillageModel'), //created model loading here
   bodyParser = require('body-parser');
-  
+
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/lakevillagedb'); 
+mongoose.connect('mongodb://localhost/lakevillagedb');
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.get('/test', (req, res) => {
+  console.log('request comming')
+  res.send(JSON.stringify({ ok: true }));
+})
 
 var routes = require('./api/routes/lakevillageRoutes'); //importing route
 routes(app); //register the route
